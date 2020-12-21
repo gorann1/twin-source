0b4f5345eabb3446982db6e896f37e49a8976376
goran <goran.novacic@gmail.com>
2020-12-21 19:34:04 +0100

    #start_graphql_success

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,25 @@ import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants'
 import mikroConfig from './mikro-orm.config'
 import express from 'express';
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql'
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  const app = express()
-  app.listen(4000, () => {
-    app.get('/', (_, res) => {
-      res.send('Zdravo');
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers:[HelloResolver],
+      validate: false
     })
-    console.log('server listening on port 4000')
+  });
+
+  apolloServer.applyMiddleware({app})
+
+  app.listen(4000, () => {
+  console.log('server listening on port 4000')
   })
 
 
